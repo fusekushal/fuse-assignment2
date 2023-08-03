55d99d39dae571a9ea966c49e1c7bb473b1e1bcf
fusekushal <kushal.bhattarai@fusemachines.com>
2023-08-03 13:32:21 +0545

    Using plugins to manage pre commit hook

--- Assignment2.py
+++ Assignment2.py
@@ -1,22 +1,26 @@
-def add(x, y): 
-    '''function to add two float numbers input by the user'''
+def add(x, y):
+    """function to add two float numbers input by the user"""
     return x + y
 
-def subtract(x, y): 
-    '''function to subtract two float numbers input by the user'''
+
+def subtract(x, y):
+    """function to subtract two float numbers input by the user"""
     return x - y
 
-def multiply(x, y): 
-    '''function to multiply two float numbers input by the user'''
-    return x * y 
+
+def multiply(x, y):
+    """function to multiply two float numbers input by the user"""
+    return x * y
+
 
 def divide(x, y):
-    '''function to divide two float numbers input by the user. 
-    It however also checks divide by zero error as it should'''
+    """function to divide two float numbers input by the user.
+    It however also checks divide by zero error as it should"""
     if y != 0:
         return x / y
     else:
-        raise ValueError("Cannot divide by zero!")              
+        raise ValueError("Cannot divide by zero!")
+
 
 def main():
     print("Choose an operation:")
@@ -30,21 +34,21 @@ def main():
     n1 = float(input("Enter first number: "))
     n2 = float(input("Enter second number: "))
 
-    if choice == '1':
+    if choice == "1":
         print(add.__doc__)
         print("Result:", add(n1, n2))
-    elif choice == '2':
+    elif choice == "2":
         print(subtract.__doc__)
         print("Result:", subtract(n1, n2))
-    elif choice == '3':
+    elif choice == "3":
         print(multiply.__doc__)
         print("Result:", multiply(n1, n2))
-    elif choice == '4':
+    elif choice == "4":
         print(divide.__doc__)
         print("Result:", divide(n1, n2))
     else:
         print("Invalid choice!!")
-        
+
 
 if __name__ == "__main__":
     main()
